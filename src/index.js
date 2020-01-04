586d2bb568f9cd3cadb635aee049de99d410da8b
Siddharth Natrajan <sidddharthvn2014@gmail.com>
2020-01-04 23:45:19 +0530

    Setup Redux

--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import configureStore from "store";
 import App from "components/App";
 import GlobalStyles from "styles/globals";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(
-  <React.Fragment>
-    <GlobalStyles />
-    <App />
-  </React.Fragment>,
-  document.getElementById("root")
-);
+const renderApp = () => {
+  const initState = {};
+  const store = configureStore(initState);
+  store.dispatch({ type: "@hnClone/@@INIT" });
+  ReactDOM.render(
+    <Provider store={store}>
+      <GlobalStyles />
+      <App />
+    </Provider>,
+    document.getElementById("root")
+  );
+};
+
+renderApp();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
