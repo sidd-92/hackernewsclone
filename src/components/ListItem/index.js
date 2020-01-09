38f1c663e39fef59c7caba5b98c029653f7bc42c
Siddharth <siddharthvishvanth.natrajan@oicapps.com>
2020-01-09 19:16:35 +0530

    Completed Redux Basic Tutorial

--- src/components/ListItem/index.js
+++ src/components/ListItem/index.js
@@ -1,4 +1,7 @@
 import React from "react";
+import TimeAgo from "react-timeago";
+import getSiteHostname from "utils/getSiteHostname";
+import getArticleLink, { HN_USER, HN_ITEM } from "utils/getArticleLink";
 import {
   Item,
   Title,
@@ -9,23 +12,27 @@ import {
 } from "./styles";
 
 const LINK_REL = "noopener noreferrer nofollow";
-
-const ListItem = () => {
+const ListItem = ({ by, kids = [], score, url, title, id, type, time }) => {
+  const site = getSiteHostname(url) || "new.ycombinator.com";
+  const link = getArticleLink({ url, id });
+  const commentUrl = `${HN_ITEM}${id}`;
+  const userUrl = `${HN_USER}${by}`;
   return (
     <Item>
-      <ExternalLink>
+      <ExternalLink href={link} rel={LINK_REL} target="_blank">
         <Title>
-          The Dev Community <Host>(gitconnected.com)</Host>
+          {title} <Host>({site})</Host>
         </Title>
       </ExternalLink>
       <Description>
-        9000 points by{" "}
-        <CommentLink href="#" rel={LINK_REL} target="_blank">
-          Test User
+        {score} points by{" "}
+        <CommentLink href={userUrl} rel={LINK_REL} target="_blank">
+          {by}
         </CommentLink>
-        1 Hour Ago{"|"}
-        <CommentLink href="#" rel={LINK_REL} target="_blank">
-          42 Comments
+        <TimeAgo date={new Date(time * 1000).toISOString()} />
+        {" | "}
+        <CommentLink href={commentUrl} rel={LINK_REL} target="_blank">
+          {kids.length} Comments
         </CommentLink>
       </Description>
     </Item>
